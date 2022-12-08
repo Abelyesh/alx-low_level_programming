fcd50aad8f73b67f3a6348b62a2b6dc3a48b6707
Abelyesh <abelyesh@gmail.com>
2022-12-08 10:50:11 -0800

    thagafav

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
-#!/bin/bash
+# !/bin/bash
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
 #include <stdio.h>
 /**
- * main - print the string in the put function
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
- */
+* main - print the string in the put function
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
