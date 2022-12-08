995cf0882019e04b45cf28ea33a44626af7614db
Abelyesh <abelyesh@gmail.com>
2022-12-08 10:53:05 -0800

    kamanabafr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 # !/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
 #include <stdio.h>
 /**
 * main - print the string in the put function
@@ -13,4 +12,3 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
