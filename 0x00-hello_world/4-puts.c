71315abd6ef1c3d605e87d75f29e41311e110746
Abelyesh <abelyesh@gmail.com>
2022-12-08 10:27:46 -0800

    space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #!/bin/bash
-#include <stdio.h>
 
+#include <stdio.h>
 /**
  *
  * * main - print the string in the put function
@@ -13,14 +13,8 @@
  *
  * */
 
-
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
