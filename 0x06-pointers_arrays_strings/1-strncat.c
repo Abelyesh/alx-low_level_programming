4195b1e327dd9ed8cf7e5459e7440d44f5d6a2a2
Abelyesh <abelyesh@gmail.com>
2022-12-21 00:13:48 -0800

    stracto

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _strncat -  concatenates two strings.
+ * @src: array with the string source
+ * @dest: pointer where the text is append
+ * @n: num of bytes to be append from src
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, i;
+
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+		;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	/*should end with a end of string char*/
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
+
