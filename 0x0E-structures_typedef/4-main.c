e12a03e69265798d2d301aa95baeb7d9e331446a
Abelyesh <abelyesh@gmail.com>
2023-01-17 01:14:46 -0800

    hanauaat

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -8,8 +8,6 @@
  
  * main - check the code
  
- * 
- 
  * Return: Always 0.
  
  */
@@ -24,8 +22,8 @@ int main(void)
 
 	my_dog = new_dog("Poppy", 3.5, "Bob");
 
-
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
 
 	return (0);
+
 }
